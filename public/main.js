076c3ae322eb1b084eea5b6d8374fa2555ca09d7
Monster0506 <12682102-monster0506@users.noreply.gitlab.com>
2024-08-26 17:59:15 -0400

    Fix more

--- public/main.js
+++ public/main.js
@@ -80,8 +80,11 @@ document.addEventListener("DOMContentLoaded", () => {
   // Load articles on page load
   const loadArticles = async () => {
     // const response = await fetch("/api/articles");
+    console.log("anything?");
     const response = await fetch(`/api/articles?sort=id&archived=${archived}`);
+    console.log(response);
     const articles = await response.json();
+    console.log("anything3?");
     renderArticles(articles);
   };
   reads.addEventListener("click", async () => {
